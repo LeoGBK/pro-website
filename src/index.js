94c2af49d8f9fe1efcbd86b047c4c6b6c118efe3
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-06-08 16:07:27 +0200

    Change to the Website Title

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./js/script.js";
+import "./js/style-switcher.js";
+import Typed from "typed.js";
+
+const typed = new Typed(".element", {
+  strings: ["Hello", "World"],
+  typeSpeed: 50
+});
+
+import "./css/index.css";
